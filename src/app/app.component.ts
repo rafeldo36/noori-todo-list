a640ac4759dab3438100c0cab3662c9e1eb4d3ab
Rafey <rafeldo36@gmail.com>
2024-07-03 00:49:49 +0530

    alll done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TodosComponent],
+  imports: [RouterOutlet,TodosComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
